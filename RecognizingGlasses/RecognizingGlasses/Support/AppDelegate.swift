d4e46b53da48f4bb1fee4d022e910e8dd6895284
yuvoz <yuvoz@ciklum.com>
2023-10-02 21:38:12 -0400

    RecognizingGlasses

---
+++ RecognizingGlasses/RecognizingGlasses/Support/AppDelegate.swift
@@ -0,0 +1,25 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+App Delegate for the RecognizingGlasses sample.
+*/
+
+import UIKit
+import ARKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+//        guard ARWorldTrackingConfiguration.isSupported else {
+//            fatalError("")
+//        }
+        
+        return true
+    }
+}
